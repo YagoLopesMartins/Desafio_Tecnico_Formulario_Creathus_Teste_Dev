caaedf68957a26862090a01e6fb8f3610b720864
Yago <yagolopes777@hotmail.com>
2021-07-18 13:49:54 -0400

    modificaca no arquivo server.js

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/cadastro', function (req, res) {
+    res.render('formulario');
+});
 
 
 app.listen(3003, () => console.log('Server is running!'));
